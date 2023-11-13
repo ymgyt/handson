18cc3df0d9577c4c8bcf7041cdcb0acadeb22b34
ymgyt <yamaguchi7073xtt@gmail.com>
2023-11-13 13:12:21 +0900

    update os1000

--- kernel/os1000/run.sh
+++ kernel/os1000/run.sh
@@ -3,13 +3,10 @@
 set -xue
 
 QEMU=qemu-system-riscv32
-CC=clang
+CC=/usr/local/Cellar/llvm/17.0.4/bin/clang
 
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
-MYL="--ld-path=/nix/store/1jfx3fx1hvdvsahhnpjppj1cpm956g08-clang-wrapper-16.0.6/bin/ld"
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c
 
-# build kernel
-$CC $CFLAGS $MYL -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
-
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
